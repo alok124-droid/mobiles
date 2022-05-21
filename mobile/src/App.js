3b875233a4691a98307c7fa0862ececfe6315971
alok124-droid <alokkumardasd2016@gmail.com>
2022-05-21 15:55:10 +0530

    mobile

---
+++ mobile/src/App.js
@@ -0,0 +1,11 @@
+
+import './App.css';
+import Mobile1 from './component/Mobile1';
+
+function App() {
+  return (
+   <Mobile1/>
+  );
+}
+
+export default App;
---
+++ mobile/src/component/Mobile1.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Mobile1 = () => {
+  return (
+    <div className="div1">
+        <h2>Mobile Operating System</h2>
+        <ul>
+            <li>Android</li>
+            <li>Blackberry</li>
+            <li>Iphone</li>
+            <li>Windows Phone</li>
+        </ul>
+        <br />
+        <br />
+        <br />
+        <h2>Mobile Manufacturers</h2>
+        <ul>
+            <li type="square">Samsung</li>
+            <li type="square">HTC</li>
+            <li>Micromax</li>
+            <li type="circle">Apple</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Mobile1
